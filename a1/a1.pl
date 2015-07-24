3ba9d87d5f9a2ad2caad22bbc9c7bac1c0ebdf59
yue <MoophyAlan@gmail.com>
2015-07-24 14:48:33 +1200

    polished

--- a1/a1.pl
+++ a1/a1.pl
@@ -1,7 +1,8 @@
 /**
  * Part 1.
  */
-output :- write('----159.202 Assignment 1----'), nl,
-		      write('ID: 12086075, Name: Liu Kuan'), nl,
-		      write('----All 4 parts solved------').
+output :-
+  write('----159.202 Assignment 1----'), nl,
+  write('ID: 12086075, Name: Liu Kuan'), nl,
+  write('----All 4 parts solved------'), nl.
 
