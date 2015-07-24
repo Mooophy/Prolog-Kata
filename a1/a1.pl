38b4c661f924bc87f3c3339d0f8341830c2d847d
yue <MoophyAlan@gmail.com>
2015-07-24 14:45:12 +1200

    modified:   a1.pl

--- a1/a1.pl
+++ a1/a1.pl
@@ -1,7 +1,7 @@
 /**
  * Part 1.
  */
-output :- 	write('----159.202 Assignment 1----'), nl,
-		write('ID: 12086075, Name: Liu Kuan'), nl,
-		write('----All 4 parts solved------').
+output :- write('----159.202 Assignment 1----'), nl,
+		      write('ID: 12086075, Name: Liu Kuan'), nl,
+		      write('----All 4 parts solved------').
 
