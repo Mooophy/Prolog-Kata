a517f0a39f8269187b56562edae6a695dff3dd7f
Yue Wang <Moophyalan@gmail.com>
2015-07-25 18:20:32 +1200

    Update a1.pl

--- a1/a1.pl
+++ a1/a1.pl
@@ -1,11 +1,7 @@
 /**
  * Part 1.
  */
-output :-
-  write('----159.202 Assignment 1----'), nl,
-  write('ID: 12086075, Name: Liu Kuan'), nl,
-  write('ID: 15242698, Name: Zhenlin Cheng'), nl,
-  write('----All 4 parts solved------'), nl.
+
 
 /**
  * Part 2.
