d4082f6d64f8c333573fdb25cad7e77ad2a965dd
yue <MoophyAlan@gmail.com>
2015-08-08 14:39:09 +1200

    update =>> 	modified:   a2.pl

--- a2/a2.pl
+++ a2/a2.pl
@@ -1,4 +1,4 @@
 %Part B
-:- op(900, xfy, =>>).
-=>>(L, R) :- once(double(L, R); double(R, L)).
-double(L, R) :- L =:= R * 2.
+:- op(700, xfy, =>>).
+=>>(L, R) :-
+  (L - 2 * R)  * (R - 2 * L) =:= 0.
