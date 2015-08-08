ac40aade46341b094b6c32d48c0ee957b67f1637
Mooophy <moophyalan@gmail.com>
2015-08-07 22:53:51 -0700

    C.c done, but seems minor bug. 	modified:   a2.pl

--- a2/a2.pl
+++ a2/a2.pl
@@ -8,12 +8,22 @@ isSingleton(List) :-
   length(List, 1).
 
 %Part C
+%C.a
 totalInList([], 0).
 totalInList([Head|Tail], Sum) :-
   totalInList(Tail, SumOfTail), Sum is Head + SumOfTail.
 
+%C.b
 count(Word, [], 0).
 count(Word, [Word|Tail], N) :-
   count(Word, Tail, NofTail), N is 1 + NofTail.
 count(Word, [OtherWord|Tail], NofTail) :-
   OtherWord \= Word, count(Word, Tail, NofTail).
+
+%C.c
+adjAve([],[]).
+adjAve([_],[]).
+adjAve([Fst, Snd | TailIn], [Ave | TailOut]) :-
+    adjAve(TailIn, TailOut), Ave is (Fst + Snd) / 2.
+
+
