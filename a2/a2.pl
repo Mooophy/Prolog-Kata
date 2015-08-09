4bd62d3b24ab0244a45223f6c8d45f962a799c95
Mooophy <moophyalan@gmail.com>
2015-08-09 00:34:03 -0700

    polished 	modified:   a2/a2.pl

--- a2/a2.pl
+++ a2/a2.pl
@@ -1,30 +1,30 @@
 %Part B
 :- op(700, xfy, =>>).
-=>>(L, R) :-
-  (L - 2 * R)  * (R - 2 * L) =:= 0.
+=>>(L, R) 
+  :-  (L - 2 * R)  * (R - 2 * L) =:= 0.
 
 :- op(700, fx, isSingleton).
-isSingleton(List) :- 
-  length(List, 1).
+isSingleton(List) 
+  :-  length(List, 1).
 
 %Part C
 %C.a
 totalInList([], 0).
-totalInList([Head|Tail], Sum) :-
-  totalInList(Tail, SumOfTail), Sum is Head + SumOfTail.
+totalInList([Head|Tail], Sum) 
+  :-  totalInList(Tail, SumOfTail), Sum is Head + SumOfTail.
 
 %C.b
 count(Word, [], 0).
-count(Word, [Word|Tail], N) :-
-  count(Word, Tail, NofTail), N is 1 + NofTail.
-count(Word, [OtherWord|Tail], NofTail) :-
-  OtherWord \= Word, count(Word, Tail, NofTail).
+count(Word, [Word|Tail], N) 
+  :-  count(Word, Tail, NofTail), N is 1 + NofTail.
+count(Word, [OtherWord|Tail], NofTail) 
+  :-  OtherWord \= Word, count(Word, Tail, NofTail).
 
 %C.c
 adjAve([],[]).
 adjAve([_],[]).
-adjAve([Fst, Snd | TailIn], [Ave | TailOut]) :-
-  adjAve(TailIn, TailOut), Ave is (Fst + Snd) / 2.
+adjAve([Fst, Snd | TailIn], [Ave | TailOut])
+  :-  adjAve(TailIn, TailOut), Ave is (Fst + Snd) / 2.
 
 %C.d
 word(one,   un).
@@ -39,8 +39,8 @@ word(nine,  neuf).
 word(ten,   dix).
 
 translate([],[]).
-translate([EH|ET], [FH|FT]) :-
-  word(EH,FH), translate(ET, FT).
+translate([EH|ET], [FH|FT]) 
+  :-  word(EH,FH), translate(ET, FT).
 
 %Part D
 item(a).
