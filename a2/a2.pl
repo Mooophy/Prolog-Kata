7783875309b8dbe77a9991880501b3dcd44eb1f1
Mooophy <moophyalan@gmail.com>
2015-08-09 00:20:24 -0700

    polished

--- a2/a2.pl
+++ a2/a2.pl
@@ -49,55 +49,34 @@ item(c).
 item(d).
 
 fillRow([X0, X1, X2, X3])
-  :-  item(X0), 
-      item(X1), 
-      item(X2), 
-      item(X3),
-      X0 \= X1, 
-      X0 \= X2, 
-      X0 \= X3, 
-      X1 \= X2, 
-      X1 \= X3, 
-      X2 \= X3.
+  :-  item(X0), item(X1), item(X2), item(X3), 
+      X0 \= X1, X0 \= X2, X0 \= X3, X1 \= X2, X1 \= X3, X2 \= X3.
 
 fillSq([R0, R1, R2, R3])
-  :-  fillRow(R0), 
-      fillRow(R1), 
-      fillRow(R2), 
-      fillRow(R3),
-      R0 \= R1,
-      R0 \= R2,
-      R0 \= R3,
-      R1 \= R2,
-      R1 \= R3,
-      R2 \= R3.
+  :-  fillRow(R0), fillRow(R1), fillRow(R2), fillRow(R3),
+      R0 \= R1, R0 \= R2, R0 \= R3, R1 \= R2, R1 \= R3, R2 \= R3.
 
 noClash([], []).
 noClash([L|Ls], [R|Rs])
-  :- L \= R, noClash(Ls, Rs).
+  :-  L \= R, noClash(Ls, Rs).
 
 testSq([R0,R1,R2,R3])
-  :- 
-    noClash(R0, R1),
-    noClash(R0, R2),
-    noClash(R0, R3),
-    noClash(R1, R2),
-    noClash(R1, R3),
-    noClash(R2, R3).
+  :-  noClash(R0, R1),  noClash(R0, R2),  noClash(R0, R3), 
+      noClash(R1, R2),  noClash(R1, R3),  noClash(R2, R3).
 
 printList([])
-  :- write('|').
+  :-  write('|').
 printList([H|T])
-  :- write('| '), write(H), write(' '), printList(T).
+  :-  write('| '), write(H), write(' '), printList(T).
 
 printRow(Row)
-  :- write('-----------------\n'), length(Row, Len), printList(Row).
+  :-  write('-----------------\n'), length(Row, Len), printList(Row).
 
 %iii
 displaySq([])
-  :- write('-----------------\n').
+  :-  write('-----------------\n').
 displaySq([H|T])
-  :- printRow(H), write('\n'), displaySq(T).
+  :-  printRow(H), write('\n'), displaySq(T).
 
 generateSq(Sq)
-  :- fillSq(Sq), testSq(Sq), displaySq(Sq). 
+  :-  fillSq(Sq), testSq(Sq), displaySq(Sq). 
